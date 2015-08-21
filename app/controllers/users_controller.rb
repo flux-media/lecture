296fbe6d438d1c0ab6efca34f82ccdc7f163691c
Lee Han Kyeol <lee0hankyeol@gmail.com>
2015-08-21 17:17:28 +0900

    Applied user_mailer templates to emails sent to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,13 +41,26 @@ class UsersController < ApplicationController
       token.user = user
       if token.save
         # First, instantiate the Mailgun Client with your API key
-        mg_client = Mailgun::Client.new(Rails.application.secrets.mailgun_key)
+        mg_client = Mailgun::Client.new(Rails.application.secrets.mailgun_key, 'bin.mailgun.net', '97d74c4f', ssl = false)
 
         # Define your message parameters
-        message_params = {:from => 'bob@sandboxdd06a1ef54af47498077a84b91a0f0a0.mailgun.org',
+        html_body = render_to_string template: 'user_mailer/reset_password',
+                                     :layout => false,
+                                     :locals => {user: user,
+                                                 url: 'http://localhost:3000/sessions/new_from_token/' +
+                                                     user.id.to_s + '/' + token.key.to_s}
+
+        text_body = render_to_string template: 'user_mailer/reset_password.text',
+                                     :layout => false,
+                                     :locals => {user: user,
+                                                 url: 'http://localhost:3000/sessions/new_from_token/' +
+                                                     user.id.to_s + '/' + token.key.to_s}
+
+        message_params = {:from => 'master@sandboxdd06a1ef54af47498077a84b91a0f0a0.mailgun.org',
                           :to => user.email,
-                          :subject => 'The Ruby SDK is awesome!',
-                          :text => 'It is really easy to send a message!'}
+                          :subject => t('title_reset_password'),
+                          :text => text_body.to_str,
+                          :html => html_body.to_str}
 
         # Send your message through the client
         mg_client.send_message 'sandboxdd06a1ef54af47498077a84b91a0f0a0.mailgun.org', message_params
